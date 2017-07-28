cceb71a6145b95785b0f604e4812bc1f20ea3c75
Charith Wickramasinghe <charithw@99x.lk>
2017-07-28 19:55:47 +0530

    minor changes

---
+++ src/component/SignupForm.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class SignupForm extends Component{
+    render() {
+
+    }
+}
+
+export default SignupForm;
